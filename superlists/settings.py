6e5347351173043a9094cbc76f25d4da2a18ef51
Yamao <yamaomaoen@gmail.com>
2014-06-02 14:26:15 +0300

    Fix test runner bug.

--- superlists/settings.py
+++ superlists/settings.py
@@ -7,7 +7,7 @@ PROJECT_ROOT = here("..")
 
 root = lambda *x: join(abspath(PROJECT_ROOT), *x)
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -30,7 +30,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['superlists.55.anvil8.com']
+ALLOWED_HOSTS = []
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
